ee68584bc55a8d92b2510963e17b8d69028f2eaa
draquio <draquio@gmail.com>
2023-09-29 15:06:25 -0400

    Agregando animación

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -1,16 +1,22 @@
-import React from "react";
+import React, {useEffect} from "react";
 import "./Layout.scss";
 import { Nav, Footer } from "../components";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
 export function Layout(props) {
   const { children } = props;
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+    });
+  }, []);
   return (
     <div className="Layout">
       <nav className="navbar">
         <Nav />
       </nav>
-      <div className="separator">
-        <div className="main">{children}</div>
-      </div>
+        <div className="main" data-aos="fade-up">{children}</div>
       <footer className="footerbar">
         <Footer />
       </footer>
